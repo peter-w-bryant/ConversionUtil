b9622a8856998dd599556e2aa2e21a9c2fc2c62a
Peter Bryant <72423203+peter-w-bryant@users.noreply.github.com>
2022-08-31 15:02:13 -0500

    Added conversion table

--- app.py
+++ app.py
@@ -10,34 +10,34 @@ def home():
     return render_template('home.html')
 
 # Binary/Decimal/Hex/Octal Converter PAGE
-@app.route('/convertDecBin', methods=['GET','POST'])
-def convertDecBin():
+@app.route('/convert', methods=['GET','POST'])
+def convert():
     if request.method == 'POST':
         if request.form['dec']:
             decimal = int(request.form['dec'], 10)
             binary = bin(decimal)[2:] 
             octdecimal = oct(decimal)[2:]
             hexdecimal = hex(decimal)[2:].upper()
-            return render_template('convertDecBin.html', decimal=decimal, binary=binary, hexdecimal=hexdecimal, octdecimal=octdecimal)
+            return render_template('convert.html', decimal=decimal, binary=binary, hexdecimal=hexdecimal, octdecimal=octdecimal)
         elif request.form['bin']:
             binary = request.form['bin']
             decimal = int(binary, 2)
             octdecimal = oct(decimal)[2:]
             hexdecimal = hex(decimal)[2:].upper()
-            return render_template('convertDecBin.html',  decimal=decimal, binary=binary, hexdecimal=hexdecimal, octdecimal=octdecimal)
+            return render_template('convert.html',  decimal=decimal, binary=binary, hexdecimal=hexdecimal, octdecimal=octdecimal)
         elif request.form['hex']:
             hexdecimal = request.form['hex']
             decimal = int(hexdecimal, 16)
             octdecimal = oct(decimal)[2:]
             binary = bin(decimal)[2:]
-            return render_template('convertDecBin.html',  decimal=decimal, binary=binary, hexdecimal=hexdecimal, octdecimal=octdecimal)
+            return render_template('convert.html',  decimal=decimal, binary=binary, hexdecimal=hexdecimal, octdecimal=octdecimal)
         elif request.form['oct']:
             octdecimal = request.form['oct']
             decimal = int(octdecimal, 8)
             binary = bin(decimal)[2:]
             hexdecimal = hex(decimal)[2:].upper()
-            return render_template('convertDecBin.html',  decimal=decimal, binary=binary, hexdecimal=hexdecimal, octdecimal=octdecimal)
-    return render_template('convertDecBin.html')
+            return render_template('convert.html',  decimal=decimal, binary=binary, hexdecimal=hexdecimal, octdecimal=octdecimal)
+    return render_template('convert.html')
 
 
 if __name__ == '__main__':
